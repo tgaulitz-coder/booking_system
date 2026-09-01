e303393d96fdf76706e34f68119d9b0784e59ad9
tgaulitz-coder <tgaulitz@gmail.com>
2026-09-01 11:46:51 +0200

    Delete booking_system directory

--- booking_system/booking-system/js/utils.js
+++
@@ -1,8 +0,0 @@
-function escapeHtml(value) {
-    return String(value ?? "")
-        .replaceAll("&", "&amp;")
-        .replaceAll("<", "&lt;")
-        .replaceAll(">", "&gt;")
-        .replaceAll('"', "&quot;")
-        .replaceAll("'", "&#039;");
-}
